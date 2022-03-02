fcc20b64a83a0cbd9f1dd0da8f27575c4323b96a
takeandrei5 <takeandrei5@gmail.com>
2022-03-02 18:50:19 +0200

    feat: added user repository interface and dummy implementation

---
+++ Database/DataAccess/UserDbOperations/Repository.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Interfaces;
+using Domain.Users;
+using SoftwareCraft.Functional;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.DataAccess.UserDbOperations;
+
+public sealed class Repository : IUserRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public Repository(AppDbContext dbContext) => _dbContext = dbContext;
+
+    public Task<Result<Maybe<User>, string>> FindUserByEmailAsync(string email)
+    {
+        throw new NotImplementedException();
+        //_dbContext.Users.Where(x => x.Email)
+    }
+}
---
+++ Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Users;
+using SoftwareCraft.Functional;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces;
+
+public interface IUserRepository
+{
+    Task<Result<Maybe<User>, string>> FindUserByEmailAsync(string email);
+}
