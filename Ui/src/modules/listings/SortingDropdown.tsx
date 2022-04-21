83b837916db54412e02ac9f9bac0ef55e13723f4
takeandrei5 <takeandrei5@gmail.com>
2022-04-21 20:10:40 +0300

    feat: added controlled state for sorting dropdown

--- Ui/src/modules/listings/SortingDropdown.tsx
+++ Ui/src/modules/listings/SortingDropdown.tsx
@@ -3,11 +3,11 @@ import { Flex } from '@chakra-ui/react';
 import { Dropdown, Typography } from '../../components/shared';
 import { SortingDropdownProps } from './types';
 
-const SortingDropdown = ({ options, onChangeHandler }: SortingDropdownProps): JSX.Element => (
+const SortingDropdown = ({ options, onChangeHandler, value }: SortingDropdownProps): JSX.Element => (
 	<Flex marginTop='1rem'>
 		<Flex alignItems='center' direction='row' marginLeft='auto' columnGap='0.625rem'>
 			<Typography variant='input'>Sort by: </Typography>
-			<Dropdown options={options} onChangeHandler={onChangeHandler} />
+			<Dropdown options={options} onChangeHandler={onChangeHandler} value={value} />
 		</Flex>
 	</Flex>
 );
