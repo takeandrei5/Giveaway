d669eee4d54104a812b04f49d1b189294364b086
Andrei ~Tache <takeandrei5@gmail.com>
2022-05-19 14:52:02 +0300

    feat: added jest and react testing library

---
+++ Ui/jest.config.js
@@ -0,0 +1,28 @@
+// jest.config.js
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+	// Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+	dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+	// Add more setup options before each test is run
+	// setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+	// if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+	moduleDirectories: ['node_modules', '<rootDir>/'],
+	testEnvironment: 'jsdom',
+	clearMocks: true,
+	moduleNameMapper: {
+		'^@api/(.*)$': '<rootDir>/api/$1',
+		'^@components/(.*)$': '<rootDir>/components/$1',
+		'^@layouts/(.*)$': '<rootDir>/layouts/$1',
+		'^@modules/(.*)$': '<rootDir>/modules/$1',
+		'^@redux/(.*)$': '<rootDir>/redux/$1',
+		'^@utils/(.*)$': '<rootDir>/utils/$1',
+	},
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
