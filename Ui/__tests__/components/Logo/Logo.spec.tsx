9ea0d1a8a833e22cdd9790ca03cf71af6d5c825e
Andrei ~Tache <takeandrei5@gmail.com>
2022-05-19 14:52:23 +0300

    tests: added tests for Logo component

---
+++ Ui/__tests__/components/Logo/Logo.spec.tsx
@@ -0,0 +1,42 @@
+import { Logo } from '@components';
+import { LogoProps } from '@components/shared/Logo/types';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+describe('Logo', () => {
+	it('should render successfully with default props', () => {
+		// Arrange
+		const props: LogoProps = { onClick: () => {} };
+
+		// Act
+		const { baseElement } = render(<Logo {...props} />);
+
+		// Assert
+		expect(baseElement).toBeTruthy();
+	});
+
+	it('should match snapshot', () => {
+		// Arrange
+		const props: LogoProps = { onClick: () => {} };
+
+		// Act
+		const component = render(<Logo {...props} />);
+
+		// Assert
+		expect(component).toMatchSnapshot();
+	});
+
+	it('should trigger onClick when the logo image is clicked successfully', async () => {
+		// Arrange
+		const props: LogoProps = {
+			onClick: jest.fn(() => {}),
+		};
+
+		// Act
+		const { getByTestId } = render(<Logo {...props} />);
+		await userEvent.click(getByTestId('logo-icon'));
+
+		// Assert
+		expect(props.onClick).toHaveBeenCalledTimes(1);
+	});
+});
