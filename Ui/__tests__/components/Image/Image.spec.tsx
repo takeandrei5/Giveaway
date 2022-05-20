58cba5d7bf505043a51b3da0d9749bb6daae9e11
Andrei ~Tache <takeandrei5@gmail.com>
2022-05-20 20:00:00 +0300

    tests: added image tests

---
+++ Ui/__tests__/components/Image/Image.spec.tsx
@@ -0,0 +1,18 @@
+import { Image } from '@components';
+import { ImageProps } from '@components/shared/Image/types';
+import { render } from '@testing-library/react';
+
+describe('Image', () => {
+	it('should match snapshot', () => {
+		// Arrange
+		const props: ImageProps = {
+			src: 'https://via.placeholder.com/150',
+		};
+
+		// Act
+		const component = render(<Image {...props} />);
+
+		// Assert
+		expect(component).toMatchSnapshot();
+	});
+});
