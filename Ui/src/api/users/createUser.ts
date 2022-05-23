c45b7f1c9d3d17761b494238eaab5abf27555edf
Andrei ~Tache <takeandrei5@gmail.com>
2022-05-23 15:32:55 +0300

    test: added createUser tests refactor: minor changes to createUser API

--- Ui/src/api/users/createUser.ts
+++ Ui/src/api/users/createUser.ts
@@ -1,6 +1,6 @@
 import axiosInstance from '../../utils/axios';
 
-const createUser = async (accessToken: string): Promise<void | undefined> => {
+const createUser = async (accessToken: string): Promise<void> => {
 	await axiosInstance.post(
 		'/users',
 		{},
