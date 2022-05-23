0bc827720470896b08260a8c52a41b61da429abf
Andrei ~Tache <takeandrei5@gmail.com>
2022-05-23 22:10:38 +0300

    refactor: switch from click to userEvent click

--- Ui/__tests__/components/Buttons/ButtonPrimary/ButtonPrimary.spec.tsx
+++ Ui/__tests__/components/Buttons/ButtonPrimary/ButtonPrimary.spec.tsx
@@ -1,5 +1,6 @@
 import { ButtonPrimary } from '@components';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 describe('ButtonPrimary', () => {
 	it('should match snapshot', () => {
@@ -9,13 +10,13 @@ describe('ButtonPrimary', () => {
 		expect(component).toMatchSnapshot();
 	});
 
-	it('should execute onClick correctly when button is clicked', () => {
+	it('should execute onClick correctly when button is clicked', async () => {
 		// Arrange
 		const onClick = jest.fn(() => {});
 
 		// Act
 		const { getByTestId } = render(<ButtonPrimary onClick={onClick} />);
-		getByTestId('button-primary').click();
+		await userEvent.click(getByTestId('button-primary'));
 
 		// Assert
 		expect(onClick).toHaveBeenCalledTimes(1);
