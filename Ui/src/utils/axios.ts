14af6af03c83a82ed22df2266837da57a47273ec
Andrei ~Tache <takeandrei5@gmail.com>
2022-05-23 12:37:43 +0300

    feat: changed date serialization for axios responses

--- Ui/src/utils/axios.ts
+++ Ui/src/utils/axios.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
 import getConfig from 'next/config';
 
 const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
@@ -14,6 +14,31 @@ const axiosCdnInstance: AxiosInstance = axios.create({
 		Authorization: `Bearer ${process.env.NEXT_PUBLIC_CDN_ACCESS_TOKEN}`,
 	},
 });
+
+axiosInstance.interceptors.response.use((response: AxiosResponse) => {
+	handleDates(response.data);
+	return response;
+});
+
+function isIsoDateString(value: unknown): boolean {
+	const isoDateFormat: RegExp = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d*)?(?:[-+]\d{2}:?\d{2}|Z)?$/;
+
+	return !!value && typeof value === 'string' && isoDateFormat.test(value);
+}
+
+export function handleDates(body: any): void {
+	if (!body || typeof body !== 'object') return;
+
+	for (const key of Object.keys(body)) {
+		const value = body[key];
+		if (isIsoDateString(value)) {
+			body[key] = new Date(value as string);
+			continue;
+		}
+
+		if (typeof value === 'object') handleDates(value);
+	}
+}
 
 export { axiosCdnInstance };
 
