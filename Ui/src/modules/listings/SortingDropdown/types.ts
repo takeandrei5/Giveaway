69e10c4f37277f8a2fc4cac0f66c155226534699
Andrei ~Tache <takeandrei5@gmail.com>
2022-05-25 20:06:30 +0300

    fix: fixed wrong name

--- Ui/src/modules/listings/SortingDropdown/types.ts
+++ Ui/src/modules/listings/SortingDropdown/types.ts
@@ -1,3 +1,3 @@
 import { DropdownProps } from '@components/Dropdown/types';
 
-export type SortingDropdownProps = DropdownProps & { id: string; name: string };
+export type SortingDropdownProps = DropdownProps;
