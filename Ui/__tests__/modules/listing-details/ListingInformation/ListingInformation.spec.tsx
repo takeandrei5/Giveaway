b0ce20af7a5503695be9906e38c50693198652ac
Andrei ~Tache <takeandrei5@gmail.com>
2022-05-25 22:14:26 +0300

    test: added listing information test

---
+++ Ui/__tests__/modules/listing-details/ListingInformation/ListingInformation.spec.tsx
@@ -0,0 +1,23 @@
+import { ListingInformation } from '@modules/listing-details/ListingInformation';
+import { ListingInformationProps } from '@modules/listing-details/ListingInformation/types';
+import { render } from '@testing-library/react';
+
+jest.mock('dateformat', () => jest.fn());
+
+describe('ListingInformation', () => {
+	it('should match snapshot', () => {
+		// Arrange
+		const props: ListingInformationProps = {
+			title: 'test-title',
+			description: 'test-description',
+			createdAt: new Date(),
+			images: ['test-image-url'],
+		};
+
+		// Act
+		const component = render(<ListingInformation {...props} />);
+
+		// Assert
+		expect(component).toBeTruthy();
+	});
+});
