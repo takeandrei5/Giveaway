9cfdd9d06b38678c4702869d423fed47ae4b3306
Andrei ~Tache <takeandrei5@gmail.com>
2022-05-25 21:50:27 +0300

    test: added owner information test

---
+++ Ui/__tests__/modules/listing-details/OwnerInformation/OwnerInformation.spec.tsx
@@ -0,0 +1,20 @@
+import { OwnerInformation } from '@modules/listing-details/OwnerInformation';
+import { OwnerInformationProps } from '@modules/listing-details/OwnerInformation/types';
+import { render } from '@testing-library/react';
+
+describe('OwnerInformation', () => {
+	it('should match snapshot', () => {
+		// Arrange
+		const props: OwnerInformationProps = {
+			name: 'John Doe',
+			email: 'test-email@test.com',
+			image: 'test-image-url',
+		};
+
+		// Act
+		const component = render(<OwnerInformation {...props} />);
+
+		// Assert
+		expect(component).toBeTruthy();
+	});
+});
