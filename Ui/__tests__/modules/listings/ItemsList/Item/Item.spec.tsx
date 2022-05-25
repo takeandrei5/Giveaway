39bcdd910844309ff67906e5e5b4c29c86e84cf5
Andrei ~Tache <takeandrei5@gmail.com>
2022-05-25 21:44:46 +0300

    test: created Item test

---
+++ Ui/__tests__/modules/listings/ItemsList/Item/Item.spec.tsx
@@ -0,0 +1,68 @@
+import { Item } from '@modules/listings/ItemsList/Item';
+import { ItemProps } from '@modules/listings/ItemsList/Item/types';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { useRouter } from 'next/router';
+
+jest.mock('dateformat', () => jest.fn());
+jest.mock('next/router', () => ({
+	...jest.requireActual('next/router'),
+	useRouter: jest.fn(),
+}));
+
+describe('Item', () => {
+	const props: ItemProps = {
+		id: '1',
+		title: 'Item 1',
+		image: 'https://via.placeholder.com/150',
+		createdAt: new Date(),
+	};
+
+	it('should match snapshot', () => {
+		// Act
+		const component = render(<Item {...props} />);
+
+		// Assert
+		expect(component).toBeTruthy;
+	});
+
+	describe('onClick', () => {
+		(useRouter as unknown as jest.Mock).mockImplementation(() => ({
+			push: jest.fn((value: string) => {}),
+		}));
+
+		it('should call onClickHandler every time is clicked', async () => {
+			// Arrange
+			const jestOnClickHandler: jest.Mock = jest.fn((value: string) => {});
+			(useRouter as unknown as jest.Mock).mockImplementation(() => ({
+				push: jestOnClickHandler,
+			}));
+
+			// Act
+			const { getByTestId } = render(<Item {...props} />);
+
+			await userEvent.click(getByTestId('item'));
+
+			// Assert
+			expect(jestOnClickHandler).toHaveBeenCalledTimes(1);
+		});
+
+		it('should redirect user to `/listings/${id}` when called', async () => {
+			// Arrange
+			const jestOnClickHandler: jest.Mock = jest.fn((value: string) => (router = value));
+			let router: string = '';
+			(useRouter as unknown as jest.Mock).mockImplementation(() => ({
+				router,
+				push: jestOnClickHandler,
+			}));
+
+			// Act
+			const { getByTestId } = render(<Item {...props} />);
+
+			await userEvent.click(getByTestId('item'));
+
+			// Assert
+			expect(router).toBe(`/listings/${props.id}`);
+		});
+	});
+});
