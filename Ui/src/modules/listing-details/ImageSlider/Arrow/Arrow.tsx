aae514f596e6acc84a882f9368dad8a9011b9569
Andrei ~Tache <takeandrei5@gmail.com>
2022-05-25 22:14:11 +0300

    fix: fixed arrow positions

--- Ui/src/modules/listing-details/ImageSlider/Arrow/Arrow.tsx
+++ Ui/src/modules/listing-details/ImageSlider/Arrow/Arrow.tsx
@@ -4,9 +4,9 @@ import { ArrowProps } from './types';
 
 const Arrow = ({ isNextArrow = false, ...rest }: ArrowProps): JSX.Element =>
 	isNextArrow ? (
-		<GrPrevious className='slick-prev slick-arrow' {...rest} />
-	) : (
 		<GrNext className='slick-next slick-arrow' {...rest} />
+	) : (
+		<GrPrevious className='slick-prev slick-arrow' {...rest} />
 	);
 
 export default Arrow;
