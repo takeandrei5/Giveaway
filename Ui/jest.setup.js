590b8a0f5998a93e2269f3a594b49574f7d7beff
Andrei ~Tache <takeandrei5@gmail.com>
2022-05-25 22:15:23 +0300

    chore: added window matchmedia inside jest setup

--- Ui/jest.setup.js
+++ Ui/jest.setup.js
@@ -3,3 +3,13 @@ global.console = {
 	warn: jest.fn(),
 	error: jest.fn(),
 };
+
+window.matchMedia =
+	window.matchMedia ||
+	function () {
+		return {
+			matches: false,
+			addListener: function () {},
+			removeListener: function () {},
+		};
+	};
