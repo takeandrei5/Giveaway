b7a7995918bc5aa3eaaaef69c41ed0f349546745
Andrei ~Tache <takeandrei5@gmail.com>
2022-05-31 20:42:17 +0300

    test: updated Header hooks test

--- Ui/__tests__/components/Header/hooks.spec.ts
+++ Ui/__tests__/components/Header/hooks.spec.ts
@@ -1,4 +1,5 @@
 import { useLogin } from '@components/Header/hooks';
+import { renderHook } from '@testing-library/react-hooks';
 import { useRouter } from 'next/router';
 import React from 'react';
 
@@ -12,20 +13,18 @@ describe('useLogin hook', () => {
 
 	beforeEach(() => (route = ''));
 
-	jest.spyOn(React, 'useCallback').mockImplementation((f) => f);
-
 	(useRouter as unknown as jest.Mock).mockImplementation(() => ({
 		route,
 		push: jest.fn((value: string) => (route = value)),
 		replace: jest.fn((value: string) => (route = value)),
 	}));
 
-	const { handleSignIn, handleSignInWithReturnTo, handleSignOut } = useLogin();
+	const { result } = renderHook(() => useLogin());
 
 	describe('handleSignIn', () => {
 		it('should redirect user to `/api/auth/login` when called', async () => {
 			// Act
-			await handleSignIn();
+			result.current.handleSignIn();
 
 			// Assert
 			expect(route).toBe('/api/auth/login');
@@ -38,7 +37,7 @@ describe('useLogin hook', () => {
 			const returnTo: string = 'test-page';
 
 			// Act
-			await handleSignInWithReturnTo(returnTo);
+			result.current.handleSignInWithReturnTo(returnTo);
 
 			// Assert
 			expect(route).toBe(`/api/auth/login?returnTo=${returnTo}`);
@@ -48,7 +47,7 @@ describe('useLogin hook', () => {
 	describe('handleSignOut', () => {
 		it('should redirect user to `/api/auth/logout` when called', async () => {
 			// Act
-			await handleSignOut();
+			result.current.handleSignOut();
 
 			// Assert
 			expect(route).toBe('/api/auth/logout');
