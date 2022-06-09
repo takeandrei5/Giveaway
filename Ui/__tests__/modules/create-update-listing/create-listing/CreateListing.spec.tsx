4eeae59fac09e49f6b59762b2b02daed7e34c170
Andrei ~Tache <takeandrei5@gmail.com>
2022-06-09 17:54:22 +0300

    test: adjusted CreateListing tests

--- Ui/__tests__/modules/create-update-listing/create-listing/CreateListing.spec.tsx
+++ Ui/__tests__/modules/create-update-listing/create-listing/CreateListing.spec.tsx
@@ -1,10 +1,15 @@
 import { CreateListing } from '@modules/create-update-listing';
+import { useCreateListing } from '@modules/create-update-listing/create-listing/hooks';
 import { render } from '@testing-library/react';
 import { useGetAccessToken } from '@utils/hooks';
 import { FieldMetaProps } from 'formik';
 
 jest.mock('@utils/hooks', () => ({
 	useGetAccessToken: jest.fn(),
+}));
+
+jest.mock('@modules/create-update-listing/create-listing/hooks', () => ({
+	useCreateListing: jest.fn(),
 }));
 
 jest.mock('@components/FormControl/hooks', () => ({
@@ -20,20 +25,29 @@ jest.mock('next/config', () => () => ({
 	},
 }));
 
+jest.mock('uuid', () => ({
+	__esModule: true,
+	v4: jest.fn().mockReturnValue('9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'),
+}));
+
 describe('CreateListing', () => {
 	it('should match snapshot', () => {
-		// // Arrange
-		// (useGetAccessToken as unknown as jest.Mock).mockImplementation(() => ({
-		// 	isFetched: true,
-		// 	data: 'test-data',
-		// }));
+		// Arrange
+		const data: string = 'test-data';
 
-		// // Act
-		// const component = render(<CreateListing />);
+		(useGetAccessToken as unknown as jest.Mock).mockImplementation(() => ({
+			isFetched: true,
+			data,
+		}));
 
-		// // Assert
-		// expect(component).toBeTruthy();
+		(useCreateListing as unknown as jest.Mock).mockImplementation(() => ({
+			formik: {},
+		}));
 
-		expect('true').toBeTruthy();
+		// Act
+		// const component = render(<CreateListing />); //todo
+
+		// Assert
+		// expect(true).toBeTruthy();
 	});
 });
